b1ca8ebc18f4619aa83da482bfd318659e7b1892
malles <plop0999>
2014-10-02 14:58:13 +0200

    coords

--- mod_roundmenu/tmpl/default.php
+++ mod_roundmenu/tmpl/default.php
@@ -31,15 +31,15 @@ defined('_JEXEC') or die;
 		<div id="rmb3" class="round_menu-button round_menu-button--center">
 				<img id="Image-Maps-Com-image-maps-2014-09-24-195240" src="/modules/mod_roundmenu/assets/images/menu_opt_center.png" alt="" usemap="#image-maps-2014-09-24-195240">
 				<map name="image-maps-2014-09-24-195240" id="ImageMapsCom-image-maps-2014-09-24-195240">
-					<area shape="rect" coords="448,448,450,450" alt="Image Map" style="outline:none;" title="Image Map" 
+					<area shape="rect" coords="358,358,360,360" alt="Image Map" style="outline:none;" title="Image Map" 
 						href="<?php echo $linkMidden; ?>" />
-					<area  alt="" title="" shape="poly" coords="223,148,195,154,171,169,156,193,150,221,156,249,171,273,195,288,223,294,251,288,275,273,290,249,296,221,290,193,275,169,251,154" style="outline:none;" target="_self"
-						href="<?php echo $linkMidden; ?>"     />
-					<area  alt="" title="" shape="poly" coords="336,48,268,162,298,198,295,252,264,291,328,411,394,360,429,287,441,202,412,115,370,66" style="outline:none;" target="_self" 
-						href="<?php echo $linkBlauw; ?>"    />
-					<area  alt="" title=""  shape="poly" coords="338,42,268,155,203,145,159,172,139,227,11,220,29,133,75,71,146,29,240,13" style="outline:none;" target="_self"
-						href="<?php echo $linkGroen; ?>" />
-					<area  alt="" title="" href="<?php echo $linkGeel; ?>" shape="poly" coords="143,222,12,218,36,333,92,394,170,433,247,438,326,413,261,292,197,298,150,269" style="outline:none;" target="_self"     />
+					<area  alt="" title="" shape="poly" coords="263,53,214,129,232,165,231,197,207,227,254,315,307,270,329,215,333,155,303,89" style="outline:none;" target="_self"
+						href="<?php echo $linkBlauw; ?>"     />
+					<area  alt="" title="" shape="poly" coords="262,49,226,33,163,30,117,41,72,66,35,120,26,175,120,176,134,139,164,121,212,132" style="outline:none;" target="_self" 
+						href="<?php echo $linkGroen; ?>"    />
+					<area  alt="" title=""  shape="poly" coords="119,176,27,178,43,249,73,292,110,316,147,329,193,332,253,315,208,225,157,231,128,212" style="outline:none;" target="_self"
+						href="<?php echo $linkGeel; ?>" />
+					<area  alt="" title="" href="<?php echo $linkMidden; ?>" shape="poly" coords="179,125,157,129,139,142,126,160,122,182,126,204,139,222,157,235,179,239,201,235,219,222,232,204,236,182,232,160,219,142,201,129" style="outline:none;" target="_self"     />
 
 				</map>
 			<img src="/modules/mod_roundmenu/assets/images/menu_opt_center-hover.png" alt="" class="hoverImg">
